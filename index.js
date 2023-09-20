88760cf66749c511b1889fd124cb6f2e7000987c
Adam Gonzales <adamgonzales1@gmail.com>
2023-09-19 19:34:45 -0600

    feature: add 'new' route for campgrounds - add POST route - add method-override dependency - update view on home page

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const PORT = 3000
 const path = require('path')
 const Campground = require('./models/campground')
+const methodOverride = require('method-override')
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -17,6 +18,8 @@ mongoose
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -24,27 +27,24 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find()
-  console.log(campgrounds)
   res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findById(id)
-  console.log(campground)
   res.render('campgrounds/details', { campground })
 })
 
-// app.get('/makecampground', async (req, res) => {
-//   const camp = new Campground({
-//     name: 'Old Timers Ridge',
-//     price: '9.99',
-//     description: 'An amazing place',
-//     location: 'Tucson, Arizona',
-//   })
-//   await camp.save()
-//   res.send(camp)
-// })
+app.post('/campgrounds', async (req, res) => {
+  const newCampground = new Campground(req.body.campground)
+  await newCampground.save()
+  res.redirect(`/campgrounds/${newCampground._id}`)
+})
 
 app.listen(PORT, () => {
   console.log(`App is listening on http://localhost:${PORT}`)
