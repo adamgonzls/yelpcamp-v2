dd06a520014c903a15cd929617962ee369a85a5d
Adam Gonzales <adamgonzales1@gmail.com>
2023-09-22 22:06:12 -0600

    feature: add bootstrap

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.render('index')
