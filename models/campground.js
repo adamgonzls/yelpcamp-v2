d17dc909274301f558a66b690f3f09058b09d47f
Adam Gonzales <adamgonzales1@gmail.com>
2023-09-26 12:01:26 -0600

    feature: add getPhotos() api call for seeding db. - update structure of views for more pleasing design - update structure of campgrounds data

--- models/campground.js
+++ models/campground.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema
 
 const CampgroundSchema = new Schema({
   name: String,
-  price: String,
+  price: Number,
   description: String,
   location: String,
+  image: String,
 })
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Campground = require('../models/campground')
 const cities = require('./cities')
 const { descriptors, places, descriptions, prices } = require('./seedHelpers')
-const campground = require('../models/campground')
+require('dotenv').config()
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -19,22 +19,44 @@ function getRandomElement(arr) {
   return randomEl
 }
 
+async function getPhotos() {
+  const response = await fetch(
+    'https://api.unsplash.com/collections/9046579/photos?per_page=100',
+    {
+      headers: {
+        Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+      },
+    }
+  )
+  const data = await response.json()
+  return data
+}
+
 const seedDB = async () => {
+  await Campground.deleteMany({})
+  const photos = await getPhotos()
   for (let i = 0; i <= 20; i++) {
     const location = getRandomElement(cities)
     const descriptor = getRandomElement(descriptors)
     const place = getRandomElement(places)
     const description = getRandomElement(descriptions)
     const price = getRandomElement(prices)
+    const randomImage = getRandomElement(photos)
+    const image = randomImage.urls.regular
     const campgroundDetails = new Campground({
       name: `${descriptor} ${place}`,
       price,
       location: `${location.city}, ${location.state}`,
       description,
+      image,
     })
+    // console.log(`image: ${image}`)
+    // console.log(campgroundDetails)
     await campgroundDetails.save()
   }
 }
+
+// getPhotos()
 
 seedDB().then(() => {
   mongoose.connection.close()
